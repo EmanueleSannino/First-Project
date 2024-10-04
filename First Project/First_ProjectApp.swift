f20659dbf98b40ecccfe5e5eb0e79b86eb9551ce
EmanueleSannino <sanninomanu06@gmail.com>
2024-10-04 17:05:12 +0200

    Initial Commit

---
+++ First Project/First_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_ProjectApp.swift
+//  First Project
+//
+//  Created by Emanuele Sannino on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct First_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ First ProjectTests/First_ProjectTests.swift
@@ -0,0 +1,16 @@
+//
+//  First_ProjectTests.swift
+//  First ProjectTests
+//
+//  Created by Emanuele Sannino on 04/10/24.
+//
+
+import Testing
+
+struct First_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
